2a7526ff8802995b3fbef04048f007acb7e63353
Romarios144 <romasavon08@gmail.com>
2024-11-09 11:47:00 +0100

    Add files via upload

---
+++ lesson76/main.js
@@ -0,0 +1,10 @@
+const firstName = 'Roma'
+const lastName = 'Semchenko'
+const age = 19
+const dateOfBirth = 27.06
+let favoriteFood = 'dumplings'
+
+
+console.log(firstName, lastName, age, 'y.o')
+console.log(dateOfBirth, '2005')
+console.log(favoriteFood)
