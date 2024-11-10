6dca4fdb33b7f0de69e3bdb53e0ff603e27b59e9
Romarios144 <romasavon08@gmail.com>
2024-11-10 18:46:07 +0100

    Add files via upload

---
+++ lesson79/main.js
@@ -0,0 +1,12 @@
+let age = 19
+
+if (age >= 18) {
+  console.log('adult')
+} else {
+  console.log('You are not yet 18')
+}
+
+let myAde = 19
+
+let access = (myAde >= 18) ? 'adult' : 'You are not yet 18'
+console.log(access)
