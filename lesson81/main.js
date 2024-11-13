82fbb3beded6533c38f077c0d31f3596916bdf76
Romarios144 <romasavon08@gmail.com>
2024-11-13 11:50:04 +0100

    Add files via upload

---
+++ lesson81/main.js
@@ -0,0 +1,17 @@
+let numbers = [14, 234, 31, 47, 56, 68, 73, 86, 91, 168];
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        console.log(numbers[i]);
+    }
+}
+
+
+
+
+
+const rainbow = ['red', 'orange', 'yellow', 'green','blue', 'indigo', 'violet']
+
+for (let i = rainbow.length - 1; i >= 0; i--) {
+  console.log(rainbow[i])
+}
