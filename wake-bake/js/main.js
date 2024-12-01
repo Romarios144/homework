6c9a4cfd501925f242d4351fbd21690d544da0b3
Romarios144 <romasavon08@gmail.com>
2024-12-01 20:58:18 +0100

    Update main.js

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -1,3 +1,5 @@
+// Бургер ============================================
+
 (function () {
 
     document.addEventListener('click', burgerInit)
@@ -10,36 +12,72 @@
         if (!burgerIcon && !burgerNavLink) return
         if (document.documentElement.clientWidth > 900) return
 
-        if (!document.body.classList.contains('body__opened-menu')) {
-            document.body.classList.add('body__opened-menu')
+        if (!document.body.classList.contains('body--opened-menu')) {
+            document.body.classList.add('body--opened-menu')
         } else {
-            document.body.classList.remove('body__opened-menu')
+            document.body.classList.remove('body--opened-menu')
         }
 
     }
 
-})()
 
-// ============================================================
 
-const modal = document.querySelector('.modal')
-const modalButton = document.querySelector('.about__img-button')
 
-modalButton.addEventListener('click', openModal)
-modal.addEventListener('click', closeModal)
+    // Модалка============================================================
 
-function openModal(e) {
-    e.preventDefault()
-    document.body.classList.toggle('body--opened-modal')
-}
 
-function closeModal(e) {
-    e.preventDefault()
+    const modal = document.querySelector('.modal')
+    const modalButton = document.querySelector('.about__img-button')
 
-    const target = e.target
+    modalButton.addEventListener('click', openModal)
+    modal.addEventListener('click', closeModal)
 
-    if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
-        document.body.classList.remove('body--opened-modal')
+    function openModal(e) {
+        e.preventDefault()
+        document.body.classList.toggle('body--opened-modal')
     }
 
-}
+    function closeModal(e) {
+        e.preventDefault()
+
+        const target = e.target
+
+        if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+            document.body.classList.remove('body--opened-modal')
+        }
+
+    }
+
+    // Табы============================================================
+
+    const tabControls = document.querySelector('.tab-controls')
+
+    tabControls.addEventListener('click', toggleTab)
+
+    function toggleTab(e) {
+
+        const tabControl = e.target.closest('.tab-controls__link')
+
+        if (!tabControl) return
+        e.preventDefault()
+        if (tabControl.classList.contains('tab-controls__link--activ')) return
+
+        const tabContentID = tabControl.getAttribute('href')
+        const tabContent = document.querySelector(tabContentID)
+        const activeControl = document.querySelector('.tab-controls__link--activ')
+        const activeContent = document.querySelector('.tab-content--show')
+
+        if (activeControl) {
+            activeControl.classList.remove('tab-controls__link--activ')
+        }
+        if (activeContent) {
+            activeContent.classList.remove('tab-content--show')
+        }
+
+        tabControl.classList.add('tab-controls__link--activ')
+        tabContent.classList.add('tab-content--show')
+
+    }
+
+
+})()
